afac8217f8ada975d3ecd70794e1fede52eaaa6f
Erica Flach <erica.flach@yahoo.com>
2021-12-01 23:36:14 -0500

    QuickSort

---
+++ Flach_Erica_QuickSort.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <cmath>
+#include <algorithm>
+
+using namespace std::chrono;
+using namespace std;
+
+void readAndParseFile(float* p);
+void quickSort(float* p);
+void recQuickSort(float* p, int left, int right);
+float medianOfThree(float* p, int left, int right);
+void swap(float* p, int dex1, int dex2);
+int partitionIt(float* p, int left, int right, double pivot);
+void printArray(float* p);
+
+string file_name;
+int array_size;
+float* arr_ptr;
+
+int main()
+{
+    cout << "What is the file name?" << endl;
+    cin >> file_name;
+    
+    cout << "How many floating-point numbers are in the file?" << endl;
+    cin >> array_size;
+    if(array_size <= 0) {
+        cout << "Invalid Size" << endl;
+        exit(0);
+    }
+
+    float arr[array_size];
+    arr_ptr = arr;
+    
+    auto start = high_resolution_clock::now();
+    
+    readAndParseFile(arr_ptr);
+    cout << "Unsorted Array: " << endl;
+    printArray(arr_ptr);
+    cout << "Sorting: " << endl;
+    quickSort(arr_ptr);
+    printArray(arr_ptr);
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop-start);
+    cout << "Execution Time: " << duration.count() << " milliseconds" << endl;
+}
+
+void readAndParseFile(float* p) {
+    fstream newFile;
+    string str;
+    newFile.open(file_name,ios::in);
+    if(newFile.is_open())
+    {
+        getline(newFile, str);
+        newFile.close();
+    }
+
+    string delimiter = " ";
+    size_t pos = 0;
+    string token;
+    int index = 0;
+    while((pos = str.find(delimiter)) != std::string::npos) {
+        token = str.substr(0,pos);
+        float token_float = stof(token);
+        p[index] = token_float;
+        index++;
+        str.erase(0, pos + delimiter.length());
+    }
+}
+
+void quickSort(float* p) {
+    recQuickSort(p, 0, array_size-1);
+}
+
+void recQuickSort(float* p, int left, int right) {
+    double median = medianOfThree(p, left, right);
+    int partition = partitionIt(p, left, right, median);
+    recQuickSort(p, left, partition-1);
+    recQuickSort(p, partition+1, right);
+}
+
+float medianOfThree(float* p, int left, int right) {
+    int center = round((left + right) / 2);
+    if(p[left] > p[center]){
+        swap(p, left, center);
+        printArray(p);
+    }
+    if(p[left] > p[right]) {
+        swap(p, left, right);
+        printArray(p);
+    }
+    if(p[center] > p[right]) {
+        swap(p, center, right);
+        printArray(p);
+    }
+    swap(p, center, right-1);
+    return p[right-1];
+}
+
+void swap(float* p, int dex1, int dex2) {
+    int temp = p[dex1];
+    p[dex1] = p[dex2];
+    p[dex2] = temp;
+}
+
+int partitionIt(float* p, int left, int right, double pivot) {
+    int leftPtr = left;
+    int rightPtr = right-1;
+    while(true) {
+        while(p[++leftPtr] < pivot)
+            ;
+        while(p[--rightPtr] > pivot)
+            ;
+        if(leftPtr >= rightPtr)
+            break;
+        else
+            swap(p, leftPtr, rightPtr);
+    }
+    swap(p, leftPtr, right-1);
+    return leftPtr;
+}
+
+void printArray(float* p) {
+    for(int i = 0; i < array_size; i++) {
+        cout << p[i] << " ";
+    }
+    cout << endl;
+}
+
+
+
+
